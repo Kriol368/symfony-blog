832ca10d52739bcc33d0b2456e3f97e3f79909a1
Kriol368 <kriol368@gmail.com>
2024-10-16 13:28:24 +0200

    Funciona todo

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine, Request $request): Response
     {
-        return $this->render('index.html.twig', []);
+        $repository = $doctrine->getRepository(Category::class);
+
+        $categories = $repository->findAll();
+
+        return $this->render('index.html.twig', ['categories' => $categories]);
     }
+
 
     #[Route('/about', name: 'about')]
     public function about(): Response
